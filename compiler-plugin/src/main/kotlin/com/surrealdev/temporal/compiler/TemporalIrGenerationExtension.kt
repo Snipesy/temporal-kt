7e373b23937c9c542aca4d4317176d998a0fb4ff
Snipesy <jessie.hoogestraat@accucodeai.com>
2026-01-18 16:03:03 -0800

    enable dep graph

--- compiler-plugin/src/main/kotlin/com/surrealdev/temporal/compiler/TemporalIrGenerationExtension.kt
+++ compiler-plugin/src/main/kotlin/com/surrealdev/temporal/compiler/TemporalIrGenerationExtension.kt
@@ -3,7 +3,6 @@ package com.surrealdev.temporal.compiler
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
-import org.jetbrains.kotlin.ir.util.dump
 import java.io.File
 
 /**
